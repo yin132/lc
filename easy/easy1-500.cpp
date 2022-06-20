8f0d9de57ac3a8475db70fa8c685a88c430e4bc3
yin132 <75343894+yin132@users.noreply.github.com>
2022-06-19 19:57:34 -0700

    added 29, 108

--- easy/easy1-500.cpp
+++ easy/easy1-500.cpp
@@ -72,6 +72,65 @@ public:
     }
 
     /*
+    26. Remove Duplicates from Sorted Array
+    
+    Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+    Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+    Return k after placing the final result in the first k slots of nums.
+
+    Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+    */
+    int removeDuplicates(vector<int>& nums)
+    {
+        // O(n), O(1)
+        int n = nums.size();
+        int p = nums[0];
+        int k = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] != p)
+            {
+                swap(nums[k], nums[i]);
+                p = nums[k];
+                k++;
+            }
+        }
+        
+        return k;
+    }
+    
+    /*
+    108. Convert Sorted Array to Binary Search Tree
+       
+    Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+    A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+    */
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBSTHelper(nums, 0, nums.size() - 1);
+    }
+    
+    TreeNode* sortedArrayToBSTHelper(vector<int>& nums, int start, int end) {
+        TreeNode* n = new TreeNode();
+        n->val = nums[(end + start)/2];
+        
+        if (start == end)
+        {
+            n->left = nullptr;
+            n->right = nullptr;
+        } else {
+            int i = (end + start)/2 -1;
+            n->left = (start > i) ? nullptr : sortedArrayToBSTHelper(nums, start, i);
+            i = (end + start)/2 + 1;
+            n->right = (i > end) ? nullptr : sortedArrayToBSTHelper(nums, i, end);            
+        }
+        return n;
+    }
+    
+    
+    /*
     206. Reverse Linked List
 
     Given the head of a singly linked list, reverse the list, and return the reversed list.
