2e720a00c6536e88e3bebf119d94be2867b97935
yin132 <75343894+yin132@users.noreply.github.com>
2022-07-02 14:04:13 -0700

    add 13, 14

--- easy/easy1-500.cpp
+++ easy/easy1-500.cpp
@@ -70,7 +70,104 @@ public:
         }
         return (r == x);
     }
+    
+    /*
+    13. Roman to Integer
+    
+    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
+    Symbol       Value
+    I             1
+    V             5
+    X             10
+    L             50
+    C             100
+    D             500
+    M             1000
+    For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+
+    Given a roman numeral, convert it to an integer.
+    */
+    int romanToInt(string s) {
+        // O(n), O(1) (but O(nm) time for m different symbols)
+        auto to_val = [] (string s) {
+            if (s == "I") return 1;
+            if (s == "V") return 5;
+            if (s == "X") return 10;
+            if (s == "L") return 50;
+            if (s == "C") return 100;
+            if (s == "D") return 500;
+            if (s == "M") return 1000;
+            return 0;
+        };
+        
+        auto check_spec = [] (string s) {
+            if (s == "IV") return 4;
+            if (s == "IX") return 9;
+            if (s == "XL") return 40;
+            if (s == "XC") return 90;
+            if (s == "CD") return 400;
+            if (s == "CM") return 900;
+            return 0;
+        };
+        
+        int n = s.size();
+        int res = 0;
+        int c;
+        int k = 0;
+        for (; k < n - 1; k++)
+        {
+            if ((c = check_spec(s.substr(k, 2))))
+            {
+                res += c;
+                k++;
+            }
+            else {
+                res += to_val(s.substr(k, 1));
+            }
+        }
+        if (k == n - 1) res += to_val(s.substr(k, 1));
+        
+        return res;
+    }
+
+    /*
+    14. Longest Common Prefix
+    
+    Write a function to find the longest common prefix string amongst an array of strings.
+
+    If there is no common prefix, return an empty string "".
+    */
+    string longestCommonPrefix(vector<string>& strs) {
+        // O(mlogn) time, O(1) heap, O(n) stack
+        auto merge2 = [] (string a, string b)->string {
+            int k = 0;
+            for (; k < a.size(); k++) {
+                if (a.substr(k, 1) != b.substr(k, 1)) return a.substr(0, k);
+            }
+            return a;
+        };
+        
+        auto divide = [&strs, &merge2] (int start, int end)->string {
+            auto divide_helper = [&strs, &merge2] (int start, int end, auto & div_rec)->string             {
+                if (end == start) return strs[start];
+
+                string a = div_rec(start, (start + end) / 2, div_rec);
+                string b = div_rec((start + end) / 2 + 1, end, div_rec);
+                return merge2(a, b);            
+            };
+            return divide_helper(start, end, divide_helper);
+        };
+        
+        return divide(0, strs.size() - 1);
+    }
+    
     /*
     26. Remove Duplicates from Sorted Array
     
